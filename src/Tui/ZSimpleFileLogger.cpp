ce2962fc2b831b1a5bdb0a4736a5f5eda4818d5d
Christoph Hueffelmann <chr@istoph.de>
2019-02-18 22:54:25 +0100

    add ZSimpleFileLogger

---
+++ src/Tui/ZSimpleFileLogger.cpp
@@ -0,0 +1,24 @@
+#include "ZSimpleFileLogger.h"
+
+#include <QFile>
+#include <QDateTime>
+
+TUIWIDGETS_NS_START
+
+namespace {
+    QString logFileName;
+
+    void fileMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+        QByteArray localMsg = (QDateTime::currentDateTimeUtc().toString(Qt::ISODate) + QStringLiteral(" ") + msg).toUtf8() + "\n";
+        QFile f(logFileName);
+        f.open(QIODevice::Append | QIODevice::WriteOnly);
+        f.write(localMsg);
+    }
+}
+
+void ZSimpleFileLogger::install(const QString &path) {
+    logFileName = path;
+    qInstallMessageHandler(fileMessageOutput);
+}
+
+TUIWIDGETS_NS_END
---
+++ src/Tui/ZSimpleFileLogger.h
@@ -0,0 +1,14 @@
+#ifndef TUIWIDGETS_ZSIMPLEFILELOGGER_INCLUDED
+#define TUIWIDGETS_ZSIMPLEFILELOGGER_INCLUDED
+
+#include <Tui/tuiwidgets_internal.h>
+
+TUIWIDGETS_NS_START
+
+namespace ZSimpleFileLogger {
+    TUIWIDGETS_EXPORT void install(const QString &path);
+}
+
+TUIWIDGETS_NS_END
+
+#endif // TUIWIDGETS_ZSIMPLEFILELOGGER_INCLUDED
