53019fa4f77a310d2ae6a7a339e3e2a8edf8d1cc
Martin Hostettler <textshell@uchuujin.de>
2021-12-22 22:46:01 +0100

    Tests: catchwrapper: Add StringMaker for QPoint and ZColor.

--- src/tests/catchwrapper.h
+++ src/tests/catchwrapper.h
@@ -4,6 +4,8 @@
 #include <QString>
 #include <QRect>
 
+#include <Tui/ZColor.h>
+
 #include "../third-party/catch.hpp"
 
 namespace Catch {
@@ -25,6 +27,33 @@ namespace Catch {
         }
     };
 
+    template<>
+    struct StringMaker<QPoint, void> {
+        static std::string convert(QPoint const& value) {
+            return QStringLiteral("(x: %0, y: %1)").arg(
+                        QString::number(value.x()),
+                        QString::number(value.y())).toStdString();
+        }
+    };
+
+    template<>
+    struct StringMaker<Tui::ZColor, void> {
+        static std::string convert(Tui::ZColor const& value) {
+            if (value.colorType() == Tui::ZColor::RGB) {
+                return QStringLiteral("(color: rgb(%0, %1, %2))").arg(QString::number(value.red()),
+                                                                      QString::number(value.green()),
+                                                                      QString::number(value.blue())).toStdString();
+            } else if (value.colorType() == Tui::ZColor::Default) {
+                return "(color: default)";
+            } else if (value.colorType() == Tui::ZColor::Terminal) {
+                return QStringLiteral("(color: %0)").arg(QString::number(static_cast<int>(value.terminalColor()))).toStdString();
+            } else if (value.colorType() == Tui::ZColor::TerminalIndexed) {
+                return QStringLiteral("(color indexed: %0)").arg(QString::number(value.terminalColorIndexed())).toStdString();
+            } else {
+                return "(invalid color)";
+            }
+        }
+    };
 }
 
 #endif
