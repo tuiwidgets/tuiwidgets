2d0d51e855f95dbbf503507ebf659c3df267fa9f
Martin Hostettler <textshell@uchuujin.de>
2022-05-27 20:25:04 +0200

    ZHBoxLayout: Move common layout support code to Layout_p.{h,cpp}

---
+++ src/Tui/Layout_p.cpp
@@ -0,0 +1,101 @@
+#include "Layout_p.h"
+
+TUIWIDGETS_NS_START
+
+namespace Private {
+
+SpacerLayoutItem::SpacerLayoutItem(int width, int height, SizePolicy sizePolicyH, SizePolicy sizePolicyV)
+    : _sizePolicyH(sizePolicyH), _sizePolicyV(sizePolicyV), _size({width, height})
+{
+}
+
+void SpacerLayoutItem::setGeometry(QRect) {
+    // Nothing to do
+}
+
+QSize SpacerLayoutItem::sizeHint() const {
+    return _size;
+}
+
+SizePolicy SpacerLayoutItem::sizePolicyH() const {
+    return _sizePolicyH;
+}
+
+SizePolicy SpacerLayoutItem::sizePolicyV() const {
+    return _sizePolicyV;
+}
+
+bool SpacerLayoutItem::isVisible() const {
+    return true;
+}
+
+bool SpacerLayoutItem::isSpacer() const {
+    return true;
+}
+
+
+void placeWidgetInCell(int x, int y, int width, int height, ZLayoutItem *w, Qt::Alignment align) {
+    QSize sizeHint = w->sizeHint().expandedTo({0, 0});
+    QRect allocation;
+
+    if (height <= 0 || width <= 0) {
+        allocation = {x, y, 0, 0};
+        w->setGeometry(allocation);
+        return;
+    }
+
+    // allocate in x
+    if (sizeHint.width() >= width) { // Not big enough anyway, just squeeze it in.
+        allocation.moveLeft(x);
+        allocation.setWidth(width);
+    } else {
+        if ((align & Qt::AlignHorizontal_Mask)
+            || (w->sizePolicyH() == SizePolicy::Fixed || w->sizePolicyH() == SizePolicy::Maximum)) {
+            // use sizeHint without expansion for aligned elements and non expanding policies.
+            allocation.setWidth(sizeHint.width());
+            if (align & Qt::AlignHCenter) {
+                int spaceRemaining = width - sizeHint.width();
+                allocation.moveLeft(x + spaceRemaining / 2);
+            } else if (align & Qt::AlignRight) {
+                allocation.moveLeft(x + width - sizeHint.width());
+            } else {
+                allocation.moveLeft(x);
+            }
+        } else {
+            // allocate everything
+            allocation.moveLeft(x);
+            allocation.setWidth(width);
+        }
+    }
+
+    // allocate in y
+    if (sizeHint.height() >= height) { // Not big enough anyway, just squeeze it in.
+        allocation.moveTop(y);
+        allocation.setHeight(height);
+    } else {
+        if ((align & Qt::AlignVertical_Mask)
+            || (w->sizePolicyV() == SizePolicy::Fixed || w->sizePolicyV() == SizePolicy::Maximum)) {
+            // use sizeHint without expansion for aligned elements and non expanding policies.
+            allocation.setHeight(sizeHint.height());
+            if (align & Qt::AlignVCenter) {
+                int spaceRemaining = height - sizeHint.height();
+                allocation.moveTop(y + spaceRemaining / 2);
+            } else if (align & Qt::AlignBottom) {
+                allocation.moveTop(y + height - sizeHint.height());
+            } else {
+                allocation.moveTop(y);
+            }
+        } else {
+            // allocate everything
+            allocation.moveTop(y);
+            allocation.setHeight(height);
+        }
+    }
+
+    // place
+    w->setGeometry(allocation);
+}
+
+}
+
+TUIWIDGETS_NS_END
---
+++ src/Tui/Layout_p.h
@@ -0,0 +1,211 @@
+#ifndef TUIWIDGETS_LAYOUT_P_INCLUDED
+#define TUIWIDGETS_LAYOUT_P_INCLUDED
+
+#include <Tui/ZLayoutItem.h>
+
+#include <Tui/tuiwidgets_internal.h>
+
+TUIWIDGETS_NS_START
+
+namespace Private {
+
+    class SpacerLayoutItem : public ZLayoutItem {
+    public:
+        explicit SpacerLayoutItem(int width, int height, SizePolicy sizePolicyH, SizePolicy sizePolicyV);
+
+    public:
+        void setGeometry(QRect r) override;
+        QSize sizeHint() const override;
+        SizePolicy sizePolicyH() const override;
+        SizePolicy sizePolicyV() const override;
+        bool isVisible() const override;
+
+        bool isSpacer() const override;
+
+    private:
+        SizePolicy _sizePolicyH;
+        SizePolicy _sizePolicyV;
+        QSize _size;
+    };
+
+    void placeWidgetInCell(int x, int y, int width, int height, ZLayoutItem *w, Qt::Alignment align);
+
+    template <typename Place, typename GetHint, typename GetPolicy, typename GetIsSpacer>
+    static void boxLayouter(Place &place, GetHint &getHint, GetPolicy &getPolicy, GetIsSpacer &getIsSpacer,
+                            int items, int totalSpace, int spacing) {
+        int hintSize = 0;
+        int numExpanding = 0;
+        int numCanGrow = 0;
+        int numSpacer = 0;
+        int shrinkableSize = 0;
+        int spacerSize = 0;
+
+        for (int i = 0; i < items; i++) {
+            int hint = std::max(getHint(i), 0);
+            hintSize += hint;
+            if (getPolicy(i) == SizePolicy::Expanding) {
+                numExpanding += 1;
+            } else if (getPolicy(i) != SizePolicy::Fixed && getPolicy(i) != SizePolicy::Maximum) {
+                numCanGrow += 1;
+            }
+            if (getPolicy(i) != SizePolicy::Fixed && getPolicy(i) != SizePolicy::Minimum) {
+                shrinkableSize += hint;
+            }
+            if (getIsSpacer(i)) {
+                numSpacer += 1;
+                spacerSize += hint;
+            }
+        }
+
+        const int spaceBetween = spacing * (items - 1 - numSpacer);
+        const int hintAndSpacing = hintSize + spaceBetween;
+
+        if (hintAndSpacing == totalSpace) {
+            int pos = 0;
+
+            for (int i = 0; i < items; i++) {
+                int size = std::max(getHint(i), 0);
+                place(pos, size, i);
+                pos += size;
+                if (!getIsSpacer(i)) {
+                    pos += spacing;
+                }
+            }
+        } else if (hintAndSpacing > totalSpace) {
+            if (hintAndSpacing - spacerSize - spaceBetween <= totalSpace) { // if removing spacing makes this fit
+                int remainingSpacingSize = spacerSize + spaceBetween;
+                int toDistribute = totalSpace - (hintAndSpacing - spacerSize - spaceBetween);
+
+                int pos = 0;
+
+                for (int i = 0; i < items; i++) {
+                    int size = std::max(getHint(i), 0);
+                    if (getIsSpacer(i)) {
+                        int allocated = remainingSpacingSize ? ((size * toDistribute + remainingSpacingSize / 2) / remainingSpacingSize) : 0;
+                        remainingSpacingSize -= size;
+                        toDistribute -= allocated;
+
+                        place(pos, allocated, i);
+                        pos += allocated;
+                    } else {
+                        place(pos, size, i);
+                        pos += size;
+
+                        int allocated = remainingSpacingSize ? ((spacing * toDistribute + remainingSpacingSize / 2) / remainingSpacingSize) : 0;
+                        remainingSpacingSize -= size;
+                        toDistribute -= allocated;
+                        pos += allocated;
+                    }
+                }
+            } else {
+                int contractionNeeded = hintAndSpacing - spacerSize - spaceBetween - totalSpace;
+                if (contractionNeeded >= shrinkableSize) {
+                    int pos = 0;
+                    for (int i = 0; i < items; i++) {
+                        if (getIsSpacer(i)) {
+                            place(pos, 0, i);
+                            continue;
+                        }
+                        int size = std::max(getHint(i), 0);
+                        if (getPolicy(i) == SizePolicy::Fixed || getPolicy(i) == SizePolicy::Minimum) {
+                            place(pos, size, i);
+                            pos += size;
+                        } else {
+                            place(pos, 0, i);
+                        }
+                    }
+                } else {
+                    int toDistribute = shrinkableSize - contractionNeeded;
+                    int remainingShrinkableSize = shrinkableSize;
+
+                    int pos = 0;
+                    for (int i = 0; i < items; i++) {
+                        if (getIsSpacer(i)) {
+                            place(pos, 0, i);
+                            continue;
+                        }
+                        int size = std::max(getHint(i), 0);
+                        if (getPolicy(i) == SizePolicy::Fixed || getPolicy(i) == SizePolicy::Minimum) {
+                            place(pos, size, i);
+                            pos += size;
+                        } else {
+                            int allocated = remainingShrinkableSize ? ((size * toDistribute + remainingShrinkableSize / 2) / remainingShrinkableSize) : 0;
+                            remainingShrinkableSize -= size;
+                            toDistribute -= allocated;
+                            place(pos, allocated, i);
+                            pos += allocated;
+                        }
+                    }
+                }
+            }
+        } else if (numExpanding) {
+            // Distribute space only into expanding items
+            int pos = 0;
+
+            const int perItemExpansion = (totalSpace - hintAndSpacing) / numExpanding;
+            int remainder = (totalSpace - hintAndSpacing) % numExpanding;
+
+            for (int i = 0; i < items; i++) {
+                int size = std::max(getHint(i), 0);
+                if (getPolicy(i) == SizePolicy::Expanding) {
+                    size += perItemExpansion;
+                    if (remainder) {
+                        remainder -= 1;
+                        size += 1;
+                    }
+                }
+
+                place(pos, size, i);
+                pos += size;
+                if (!getIsSpacer(i)) {
+                    pos += spacing;
+                }
+            }
+        } else {
+            // Distribute space into items that can grow, if none just not allocate remaining space
+
+            int pos = 0;
+
+            const int perItemExpansion = numCanGrow ? (totalSpace - hintAndSpacing) / numCanGrow : 0;
+            int remainder = numCanGrow ? (totalSpace - hintAndSpacing) % numCanGrow : 0;
+
+            for (int i = 0; i < items; i++) {
+                int size = std::max(getHint(i), 0);
+                if (getPolicy(i) != SizePolicy::Fixed
+                    && getPolicy(i) != SizePolicy::Maximum) {
+                    size += perItemExpansion;
+                    if (remainder) {
+                        remainder -= 1;
+                        size += 1;
+                    }
+                }
+
+                place(pos, size, i);
+                pos += size;
+                if (!getIsSpacer(i)) {
+                    pos += spacing;
+                }
+            }
+        }
+    }
+
+    inline SizePolicy mergePolicy(SizePolicy current, SizePolicy itemPolicy) {
+        if (itemPolicy == SizePolicy::Fixed) {
+            // Nothing to do
+        } else if (itemPolicy == SizePolicy::Minimum) {
+        } else if (itemPolicy == SizePolicy::Maximum) {
+        } else if (itemPolicy == SizePolicy::Preferred) {
+            if (current != SizePolicy::Expanding) {
+                current = SizePolicy::Preferred;
+            }
+        } else if (itemPolicy == SizePolicy::Expanding) {
+            current = SizePolicy::Expanding;
+        }
+        return current;
+    }
+
+}
+
+TUIWIDGETS_NS_END
+
+#endif // TUIWIDGETS_LAYOUT_P_INCLUDED
