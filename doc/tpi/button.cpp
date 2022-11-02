0cacd8db7c895e6fe2bbbf024b71fa2897d9a39d
Christoph Hueffelmann <chr@istoph.de>
2022-11-02 15:18:43 +0100

    doc: Add tpi-image-builder.

---
+++ doc/tpi/button.cpp
@@ -0,0 +1,46 @@
+#include "button.h"
+
+void button() {
+    Tui::ZTerminal terminal (Tui::ZTerminal::OffScreen(40,40));
+
+    //terminal.resize(10,10);
+    //ImageBuilder root;
+    Tui::ZRoot root;
+    terminal.setMainWidget(&root);
+    root.setGeometry({0,0,10,10});
+    root.setFocus();
+
+    Tui::ZWindow win(&root);
+    win.setGeometry({0,0,10,10});
+    win.setFocus();
+    win.setBorderEdges({});
+
+    //button
+    {
+        Tui::ZButton button(&win);
+        button.setGeometry({0,1,10,1});
+        button.setText("Button");
+
+        export_tpi(&terminal, "button", 0,0,10,3);
+    }
+
+    //focus
+    {
+        Tui::ZButton button(&win);
+        button.setGeometry({0,1,10,1});
+        button.setText("Button");
+        button.setFocus();
+
+        export_tpi(&terminal, "button-focus", 0,0,10,3);
+    }
+
+    //disabled
+    {
+        Tui::ZButton button(&win);
+        button.setGeometry({0,1,10,1});
+        button.setText("Button");
+        button.setEnabled(false);
+
+        export_tpi(&terminal, "button-disable", 0,0,10,3);
+    }
+}
---
+++ doc/tpi/tpi-image-builder.cpp
@@ -0,0 +1,48 @@
+#include <QCoreApplication>
+
+#include <Tui/ZButton.h>
+#include <Tui/ZCheckBox.h>
+#include <Tui/ZColor.h>
+#include <Tui/ZCommandNotifier.h>
+#include <Tui/ZHBoxLayout.h>
+#include <Tui/ZInputBox.h>
+#include <Tui/ZImage.h>
+#include <Tui/ZLabel.h>
+#include <Tui/ZMenu.h>
+#include <Tui/ZMenuItem.h>
+#include <Tui/ZMenubar.h>
+#include <Tui/ZPainter.h>
+#include <Tui/ZPalette.h>
+#include <Tui/ZRadioButton.h>
+#include <Tui/ZShortcut.h>
+#include <Tui/ZSymbol.h>
+#include <Tui/ZTerminal.h>
+#include <Tui/ZTextLayout.h>
+#include <Tui/ZTextMetrics.h>
+#include <Tui/ZVBoxLayout.h>
+#include <Tui/ZWindow.h>
+
+#include "button.h"
+//#include "tpi-image-builder.h"
+
+#include <Tui/ZRoot.h>
+
+void export_tpi(Tui::ZTerminal *terminal, QString name, int x, int y, int w, int h) {
+    terminal->forceRepaint();
+    Tui::ZImage img_with_terminal = terminal->grabCurrentImage();
+    img_with_terminal.painter().setForeground(0,0,{0xff,0xff,0xff});
+
+    Tui::ZImage img(terminal,w,h);
+    img.painter().drawImage(0,0,img_with_terminal, x, y, w, h);
+    img.painter().setForeground(0,0,{0xff,0xff,0xff});
+
+    img.save(name +".tpi");
+}
+
+int main(int argc, char **argv) {
+    QCoreApplication app(argc, argv);
+
+    button();
+
+    return 0; // app.exec();
+}
