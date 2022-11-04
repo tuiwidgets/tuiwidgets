7648b159791b8fb81fb379ec427f571433c54edb
Christoph Hueffelmann <chr@istoph.de>
2022-11-04 18:45:01 +0100

    doc: build.sh: Add install info.

--- doc/build.sh
+++ doc/build.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [ ! -e ../../sphinx-v5/bin/sphinx-build ]; then
+    echo "pleas install www.sphinx-doc.org to ../../sphinx-v5"
+    exit 1
+fi
+
 ../../sphinx-v5/bin/sphinx-build -b html . _build -a -d _doctrees -n -E
 
 ./sphinxlint.py --disable "trailing-whitespace" --enable default-role
